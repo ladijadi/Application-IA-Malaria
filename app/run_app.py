0f5d8d9a3bb670bbbd41d48b871e6cb1c2813b3f
Khady Diagne <khadydiagne18@gmail.com>
2024-12-10 12:20:25 +0100

    commit app flask

--- app/run_app.py
+++ app/run_app.py
@@ -1,10 +1,10 @@
-from flask import Flask, request, jsonify
 import tensorflow as tf
 import numpy as np
 from PIL import Image
+from flask import Flask, request, render_template, jsonify
 
 app = Flask(__name__)
-model = tf.keras.models.load_model("model/malaria_model.h5")
+model = tf.keras.models.load_model("app/model/malaria_model.keras")
 
 def preprocess_image(image):
     """Prétraitement de l'image téléchargée."""
@@ -22,10 +22,15 @@ def predict():
         image = Image.open(file.stream)
         processed_image = preprocess_image(image)
         prediction = model.predict(processed_image)
-        label = "Infectée" if np.argmax(prediction) == 1 else "Non infectée"
+        label = "Infectée" if prediction < 0.5 else "Non infectée"
         return jsonify({"prediction": label})
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+    
+
+@app.route("/", methods=["GET"])
+def home():
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
